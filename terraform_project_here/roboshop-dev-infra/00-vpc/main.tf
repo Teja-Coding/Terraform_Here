d152cd171868b00ba49da4f0dcadfa1f8adfde40
Teja Coding <dropteja@gmail.com>
2025-10-27 23:17:02 +0530

    done with sg

---
+++ terraform_project_here/roboshop-dev-infra/00-vpc/main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+    source = "git::https://github.com/Teja-Coding/Terraform_Here.git//terraform_project_here/my_vpc_creation?ref=main"
+vpc_cidr = var.vpc_cidr
+project_name = var.project_name
+enviroment = var.enviroment
+vpc_tags = var.vpc_tags
+
+# public subnets
+public_subnet_cidrs = var.public_subnet_cidrs
+
+# private subnets
+private_subnet_cidrs = var.private_subnet_cidrs
+
+#database subnets
+database_subnet_cidrs = var.database_subnet_cidrs
+
+is_peering_required = true
+}
---
+++ terraform_project_here/roboshop-dev-infra/10-sg/variable.tf
@@ -0,0 +1,6 @@
+variable "project_name" {
+  default = "roboshop"
+}   
+variable "enviroment" {
+  default = "dev"
+}
